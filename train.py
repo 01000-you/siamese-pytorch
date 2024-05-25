937ee53329f1e3106bf705bcdced95905561ec92
01000you <odoetr@naver.com>
2024-05-25 23:03:53 +0900

    Add support for multiple devices and optimize data loading
    - Integrated support for multiple devices (CPU, CUDA, and MPS) for flexible computation.
    - Added command line flag for device specification and refactored device selection logic to automatically detect and use available hardware.
    - Adjusted data loader worker configuration based on device type to optimize performance.

--- train.py
+++ train.py
@@ -1,13 +1,9 @@
 import torch
 import pickle
-import torchvision
-from torchvision import transforms
-import torchvision.datasets as dset
 from torchvision import transforms
 from mydataset import OmniglotTrain, OmniglotTest
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
-import matplotlib.pyplot as plt
 from model import Siamese
 import time
 import numpy as np
@@ -21,12 +17,10 @@ writer = SummaryWriter()
 if __name__ == '__main__':
 
     Flags = gflags.FLAGS
-    # gflags.DEFINE_bool("cuda", False, "use cuda")
     gflags.DEFINE_string("train_path", "omniglot/python/images_background", "training folder")
     gflags.DEFINE_string("test_path", "omniglot/python/images_evaluation", 'path of testing folder')
     gflags.DEFINE_integer("way", 20, "how much way one-shot learning")
     gflags.DEFINE_string("times", 400, "number of samples to test accuracy")
-    gflags.DEFINE_integer("workers", 4, "number of dataLoader workers")
     gflags.DEFINE_integer("batch_size", 128, "number of batch size")
     gflags.DEFINE_float("lr", 0.00006, "learning rate")
     gflags.DEFINE_integer("show_every", 10, "show result after each show_every iter.")
@@ -35,7 +29,10 @@ if __name__ == '__main__':
     gflags.DEFINE_integer("max_iter", 50000, "number of iterations before stopping")
     gflags.DEFINE_string("log_path", "./tensorboard", "path to save logs")
     gflags.DEFINE_string("model_path", "models", "path to store model")
-
+    gflags.DEFINE_string("device", "cuda",
+                         "Specifies the device to be used for computation. "
+                         "'cuda' indicates the use of an NVIDIA GPU, 'cpu' indicates CPU usage, "
+                         "and 'mps' indicates the use of NVIDIA Multi-Process Service (MPS) for GPU sharing.")
     Flags(sys.argv)
 
     writer = SummaryWriter(Flags.log_path)
@@ -46,27 +43,32 @@ if __name__ == '__main__':
         transforms.ToTensor()
     ])
 
+    if Flags.device == 'mps':
+        device = torch.device("mps" if torch.backends.mps.is_built() else "cpu")
+    elif Flags.device == 'cuda':
+        # get gpu ids
+        gpu_count = torch.cuda.device_count()
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f'use gpu : {device}')  # todo : change logger
 
     # train_dataset = dset.ImageFolder(root=Flags.train_path)
     # test_dataset = dset.ImageFolder(root=Flags.test_path)
 
     trainSet = OmniglotTrain(Flags.train_path, transform=data_transforms)
-    testSet = OmniglotTest(Flags.test_path, transform=transforms.ToTensor(), times = Flags.times, way = Flags.way)
-    testLoader = DataLoader(testSet, batch_size=Flags.way, shuffle=False, num_workers=Flags.workers)
-
-    trainLoader = DataLoader(trainSet, batch_size=Flags.batch_size, shuffle=False, num_workers=Flags.workers)
+    testSet = OmniglotTest(Flags.test_path, transform=transforms.ToTensor(),
+                           times=Flags.times, way=Flags.way)
+    testLoader = DataLoader(testSet, batch_size=Flags.way,
+                            shuffle=False, num_workers=gpu_count if device.type == 'cuda' else 1)
+    trainLoader = DataLoader(trainSet, batch_size=Flags.batch_size,
+                             shuffle=False, num_workers=gpu_count if device.type == 'cuda' else 1)
 
     loss_fn = torch.nn.BCEWithLogitsLoss(size_average=True)
+
     net = Siamese()
-
-    if torch.backends.mps.is_built():
-        print('use gpu : mps')
-        mps_device = torch.device("mps")
-        net.to(mps_device)
-
+    net.to(device)
     net.train()
 
-    optimizer = torch.optim.Adam(net.parameters(),lr = Flags.lr )
+    optimizer = torch.optim.Adam(net.parameters(), lr=Flags.lr)
     optimizer.zero_grad()
 
     train_loss = []
@@ -77,10 +79,7 @@ if __name__ == '__main__':
     for batch_id, (img1, img2, label) in enumerate(trainLoader, 1):
         if batch_id > Flags.max_iter:
             break
-        if torch.backends.mps.is_built():
-            img1, img2, label = Variable(img1.to(mps_device)), Variable(img2.to(mps_device)), Variable(label.to(mps_device))
-        else:
-            img1, img2, label = Variable(img1), Variable(img2), Variable(label)
+        img1, img2, label = Variable(img1.to(device)), Variable(img2.to(device)), Variable(label.to(device))
         optimizer.zero_grad()
         output = net.forward(img1, img2)
         loss = loss_fn(output, label)
@@ -96,9 +95,7 @@ if __name__ == '__main__':
         if batch_id % Flags.test_every == 0:
             right, error = 0, 0
             for _, (test1, test2) in enumerate(testLoader, 1):
-                if torch.backends.mps.is_built():
-                    test1, test2 = test1.to(mps_device), test2.to(mps_device)
-                test1, test2 = Variable(test1), Variable(test2)
+                test1, test2 = Variable(test1.to(device)), Variable(test2.to(device))
                 output = net.forward(test1, test2).data.cpu().numpy()
                 pred = np.argmax(output)
                 if pred == 0:
